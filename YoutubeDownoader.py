8f02bd890fa3882da22448d05ac648cb60cb4260
JohnTranTh <156347109+JohnTranTh@users.noreply.github.com>
2024-01-11 17:01:34 -0500

    Added directory changing and more info printing

--- YoutubeDownoader.py
+++ YoutubeDownoader.py
@@ -1,30 +1,42 @@
 from pytube import YouTube
 
-def download_video(url, audio_only):
-    video = YouTube(url)
+def download_video(url, output_directory, audio_only):
+    try:
+        video = YouTube(url)
 
-    if audio_only:
-        audio_stream = video.streams.filter(only_audio=True).first()
-        print_file_info(video)
-        audio_stream.download()
-    else:
-        video_stream = video.streams.get_highest_resolution()
-        print_file_info(video)
-        video_stream.download()
+        if audio_only:
+            audio_stream = video.streams.filter(only_audio=True).first()
+            print_file_info(video)
+            print(str(audio_stream.filesize_mb) + "MBs")
+            audio_stream.download(output_path = output_directory)
+            print("Download Successful")
+        else:
+            video_stream = video.streams.get_highest_resolution()
+            print_file_info(video)
+            print(str(video_stream.filesize_mb) + "MBs")
+            video_stream.download(output_path = output_directory)
+            print("Download Successful")
+    except:
+        print("Download Failed")
 
 def print_file_info(video):
-    filesize = video.streams.get_highest_resolution().filesize_mb
-    
+    print("Video Information:")
     print("Title: " + video.title)
+    print("Author: " + video.author)
     print("Length: " + str(video.length))
-    print(str(filesize) + "MBs")
 
 if __name__ == "__main__":
-    video_url = input("Enter a Youtube URL")
+    video_url = input("Enter a Youtube URL: ")
     audio_only = input("Do you want only audio (enter y if yes): ").lower()
+
     if audio_only == 'y':
         audio_only = True
     else:
         audio_only = False
-    download_video(video_url, audio_only)
-    print("File Downloaded")
+
+    output_location = input("Enter the location where you wish for the file to be saved (leave blank for current directory): ").strip()
+
+    if output_location == ' ':
+        output_location = '.'
+
+    download_video(video_url, output_location, audio_only)
